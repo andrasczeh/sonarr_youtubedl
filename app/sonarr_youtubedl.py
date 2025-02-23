7f9eb9798ed2454b85035c2e1fc92bca1a6893f4
aczeh <andras.czeh@gmail.com>
2025-02-24 00:04:12 +0100

    Revert to "webpage_url"

--- app/sonarr_youtubedl.py
+++ app/sonarr_youtubedl.py
@@ -365,11 +365,11 @@ class SonarrYTDL(object):
                 try:
                     titles = [entry['title'].lower() for entry in result['entries']]
                     index = find_best_match_index(titles, name.lower())
-                    video_url = result['entries'][index].get('url')
+                    video_url = result['entries'][index].get('webpage_url')
                 except Exception as e:
                     logger.error(e)
             else:
-                video_url = result.get('url')
+                video_url = result.get('webpage_url')
             if playlist == video_url:
                 return False, ''
             if video_url is None:
